7387ed8932f3205e29affa65fdfbce22f8efcd67
rd08111997 <ajay.renu02@gmail.com>
2017-02-19 06:28:47 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,37 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleone ={ 
+    title:'article one',
+    heading:' article one',
+    question:'who is babbu',
+    description:'<p>let me tell you that who is babbu</p>'
+    };
+function create_dummy(article)
+{
+    var title = article.title;
+    var heading = article.heading;
+    var question = article.question;
+    var description = article.description;
+     var html_dummy = 
+    `<!doctype html>
+    <html>
+    <head>
+    <title>${title}</title>    
+    </head>
+    <body>
+        <a href="/">home</a>
+        <hr/>
+        <h1>${heading}</h1>
+        <h3>${question}</h3>
+        ${description}
+         
+    </body>
+    </html>`;
+return html_dummy;
+}
+    
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -17,7 +48,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-one', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(create_dummy(article_one));
 });
 
 
