5a922a0b0761d60810a3e6b9d898fce56d558ff3
rd08111997 <ajay.renu02@gmail.com>
2017-02-19 23:35:46 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,11 @@ console.log('Loaded!');
 var marginLeft = 0;
 var img = document.getElementById('madi');
 function movert(){
-  if(marginLeft==900)
+  if(marginLeft<=900)
+  {
   marginLeft = marginLeft+1;
   img.style.marginLeft = marginLeft + 'px';
- 
+  }
 }
 img.onclick = function(){
     var time = setInterval(movert,10);
