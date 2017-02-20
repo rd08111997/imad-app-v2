218d124b704711e23adb9a56f2531277c4d6b89b
rd08111997 <ajay.renu02@gmail.com>
2017-02-19 22:52:39 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,6 +55,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/:articleName', function (req,res) {
    var articleName = req.params.articleName;
    res.send(create_dummy(article[articleName]));
