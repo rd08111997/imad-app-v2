1af869600aea5bd96d94759b19b7d89504094a1d
rd08111997 <ajay.renu02@gmail.com>
2017-02-21 07:46:19 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,8 @@ var button = document.getElementById("button");
     button.onclick = function(){
        var request = new XMLHttpRequest();
        request.onreadystatechange = function(){
-       if(httpRequest.readyState === XMLHttpRequest.DONE){
-         if(httpRequest.status === 200){
+       if(request.readyState === XMLHttpRequest.DONE){
+         if(request.status === 200){
           var count = reuest.responseText;   
           var span  = document.getElementById("countno");
           span.innerHTML = count.toString();        
