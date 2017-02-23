7ff6fa7c963d34fd0627d64fef01049b8a8edf0d
rd08111997 <ajay.renu02@gmail.com>
2017-02-23 03:05:32 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -93,3 +93,27 @@ var button = document.getElementById("button");
        
         };
         
+         $("#comsubmit").click(function(){
+        var request = new XMLHttpRequest();
+       request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE){
+         if(request.status === 200){
+         var list = request.responseText;
+          list = JSON.parse(list);
+        var k='';
+        for( var i=0;i<list.length;i++){
+        k += '<li>' + list[i] + '</li>';
+        }
+           var ul = document.getElementById("commentlist"); 
+           ul.innerHTML = k; 
+            
+             
+         }
+           
+       }
+ var nameInput = document.getElementById("comment");
+       var namer = nameInput.value;
+ request.open('GET','http://rd08111997.imad.hasura-app.io/comments?commentno='+namer,true);
+       request.send(null);
+       };   
+    });
