9f4a58c5a08bd20b142687b50afc41fe6fc5a287
rd08111997 <ajay.renu02@gmail.com>
2017-03-09 02:00:58 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/testdb', function (req, res) {
      { res.status(500).send(err.toString());}
      else
      {
-         res.send(JSON.stringify(result));
+         res.send(JSON.stringify(result.rows));
          
      }
       
